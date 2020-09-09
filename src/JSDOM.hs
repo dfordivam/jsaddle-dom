81ec60f47950f163b96b0e52d3d6cddda95ec4c0
Divam <dfordivam@gmail.com>
2020-09-09 18:07:22 +0900

    Move animationFrameHandlerVar here, as it is no longer in jsaddle state Also cleanup for APIs removed from jsaddle

--- src/JSDOM.hs
+++ src/JSDOM.hs
@@ -7,15 +7,9 @@ module JSDOM (
 , currentWindowUnchecked
 , currentDocument
 , currentDocumentUnchecked
-, syncPoint
-, syncAfter
-, waitForAnimationFrame
-, nextAnimationFrame
 , AnimationFrameHandle
 , inAnimationFrame
 , inAnimationFrame'
-, catch
-, bracket
 ) where
 
 #ifdef ghcjs_HOST_OS
@@ -26,21 +20,18 @@ import JavaScript.Web.AnimationFrame (AnimationFrameHandle, inAnimationFrame)
 #else
 import Control.Monad (void, forM_, when)
 import Control.Monad.IO.Class (MonadIO(..))
-import Control.Concurrent.MVar (putMVar, takeMVar)
-import Language.Javascript.JSaddle.Types (JSContextRef(..))
+import Control.Concurrent.MVar (MVar, newMVar, modifyMVar, swapMVar)
+import Language.Javascript.JSaddle.Types (JSContextRef(..), askJSM)
 import Language.Javascript.JSaddle.Object (freeFunction, jsg)
-import Language.Javascript.JSaddle.Monad (askJSM)
 import JSDOM.Types
        (Callback(..), RequestAnimationFrameCallback(..), FromJSVal(..),
         MonadDOM, liftDOM, Document(..), Window(..), JSM, JSContextRef(..))
 import JSDOM.Generated.RequestAnimationFrameCallback
        (newRequestAnimationFrameCallbackSync)
 import JSDOM.Generated.Window (requestAnimationFrame)
+import System.IO.Unsafe (unsafePerformIO)
 #endif
 import GHCJS.Concurrent (OnBlocked(..))
-import Language.Javascript.JSaddle
-       (syncPoint, syncAfter, waitForAnimationFrame,
-        nextAnimationFrame, catch, bracket)
 
 currentWindow :: MonadDOM m => m (Maybe Window)
 currentWindow = liftDOM $ jsg ("window" :: String) >>= fromJSVal
@@ -58,6 +49,10 @@ currentDocumentUnchecked = liftDOM $ jsg ("document" :: String) >>= fromJSValUnc
 
 data AnimationFrameHandle = AnimationFrameHandle
 
+animationFrameHandlerVar :: MVar [Double -> JSM ()]
+animationFrameHandlerVar = unsafePerformIO $ newMVar []
+{-# NOINLINE animationFrameHandlerVar #-}
+
 {- |
      Run the action in an animationframe callback. The action runs in a
      synchronous thread, and is passed the high-performance clock time
@@ -67,22 +62,21 @@ inAnimationFrame :: OnBlocked       -- ^ what to do when encountering a blocking
                  -> (Double -> JSM ())  -- ^ the action to run
                  -> JSM AnimationFrameHandle
 inAnimationFrame _ f = do
-    handlersMVar <- animationFrameHandlers <$> askJSM
-    -- Take the list of pending animation fram handlers
-    handlers <- liftIO $ takeMVar handlersMVar
     -- Add this handler to the list to be run by the callback
-    liftIO $ putMVar handlersMVar (f : handlers)
+    -- It is important to do sync here, otherwise we may end up having race
+    -- liftIO does a sync internally
+    wasEmpty <- liftIO $ modifyMVar animationFrameHandlerVar $ \old ->
+      pure (f : old, null old)
     -- If this was the first handler added set up a callback
     -- to run the handlers in the next animation frame.
-    when (null handlers) $ do
+    when wasEmpty $ do
         win <- currentWindowUnchecked
         rec cb@(RequestAnimationFrameCallback (Callback fCb)) <- newRequestAnimationFrameCallbackSync $ \t -> do
               -- This is a one off handler so free it when it runs
               freeFunction fCb
               -- Take the list of handers and empty it
-              handlersToRun <- liftIO $ takeMVar handlersMVar
-              liftIO $ putMVar handlersMVar []
-              -- Exectute handlers in the order 
+              handlersToRun <- liftIO $ swapMVar animationFrameHandlerVar []
+              -- Exectute handlers in the order
               forM_ (reverse handlersToRun) (\handler -> handler t)
         -- Add the callback function
         void $ requestAnimationFrame win cb
